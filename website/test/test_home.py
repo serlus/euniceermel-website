f56cc7a837ed275be198070c326167a255f23221
Sergio Casas <sergio.fasters@singulare.com.br>
2022-07-17 00:30:55 -0300

    change GA 2

--- website/test/test_home.py
+++ website/test/test_home.py
@@ -1,6 +1,15 @@
 from django.test import Client
+from django.urls import reverse
+import pytest
+from website.django_assertions import assert_contains
 
+@pytest.fixture
+def resp(client):
+    resp = client.get(reverse('base:home'))
+    return resp
 
-def test_status_code(client: Client):
-    resp = client.get('/')
-    return resp.status_code == 200
+def test_status_code(resp):
+    assert resp.status_code == 200
+
+def test_title(resp):
+    assert_contains(resp, '<title>Eunice Ermel</title>')
