90dd290e5fde8af0834bd73e022aad813817bb5a
Serlus <63012359+serlus@users.noreply.github.com>
2022-07-30 16:38:30 -0300

    04 (#8)
    * change GA
    
    * change GA 2
    
    * change version python in CI
    
    * fix flake8
    
    Co-authored-by: Sergio Casas <sergio.fasters@singulare.com.br>

--- website/test/test_home.py
+++ website/test/test_home.py
@@ -1,6 +1,17 @@
-from django.test import Client
+from django.urls import reverse
+import pytest
+from website.django_assertions import assert_contains
 
 
-def test_status_code(client: Client):
-    resp = client.get('/')
-    return resp.status_code == 200
+@pytest.fixture
+def resp(client):
+    resp = client.get(reverse('base:home'))
+    return resp
+
+
+def test_status_code(resp):
+    assert resp.status_code == 200
+
+
+def test_title(resp):
+    assert_contains(resp, '<title>Eunice Ermel</title>')
